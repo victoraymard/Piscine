b91dea85376ac156df1a60dd3b8041a36f312ef6
victor_aymard <victor.aymard@edu.ece.fr>
2018-04-04 15:10:47 +0200

    nombre individus remplacé par biomasse dans ressource

--- Projet_Piscine/Reservoir.h
+++ Projet_Piscine/Reservoir.h
@@ -5,7 +5,7 @@
 class Reservoir: public Vertex   ///Completement identique à class Espece pour linstant...
 {
 private:
-    int m_nombre_individus;
+    int m_biomasse;
     int m_capacite_de_portage;
     float m_rythme_de_croissance;
     std::string m_nom;
@@ -14,13 +14,13 @@ public:
     ~Reservoir();
 
 ///GETTER
-int getnombre_individus();
+int getbiomasse();
 int getcapacite_de_portage();
 float getrythme_de_croissance();
 std::string getnom();
 
 ///SETTER
-void setnombre_individus(int nombre_individus);
+void setbiomasse(int biomasse);
 void setcapacite_de_portage(int capacite_de_portage);
 void setrythme_de_croissance(float rythme_de_croissance);
 void setnom(std::string nom);
